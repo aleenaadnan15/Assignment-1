4ee78febfa8c868dcc6c4b82afcbd33e16242f3a
Aleena Adnan <50289794+aleenaadnan15@users.noreply.github.com>
2019-06-21 21:57:01 +0500

    Get a new string

---
+++ string3.py
@@ -0,0 +1,9 @@
+
+string = input('enter any string:')
+cons = 'is it ok'
+if string[:1] == cons:
+    print('the given string is unchanged!' + '\n'+ string)
+elif 'is it fresh':
+    print('the given string is unchanged!' + '\n' + string)
+else:
+    print('new string' ,cons, string )
